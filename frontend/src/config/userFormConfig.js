2bc5b9f77ad00388be8166002a5cca225eed2990
Logan251999 <150794954+Logan251999@users.noreply.github.com>
2026-02-17 23:33:29 +0530

    Delete frontend directory

--- frontend/src/config/userFormConfig.js
+++
@@ -1,6 +0,0 @@
-export const userFormFields = [
-  { name: "firstName", label: "First Name", type: "text", validator: "nameValidator" },
-  { name: "lastName", label: "Last Name", type: "text", validator: "nameValidator" },
-  { name: "phone", label: "Phone", type: "text", validator: "numberValidator" },
-  { name: "email", label: "Email", type: "email", validator: "emailValidator" },
-];
